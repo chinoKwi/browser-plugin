9587ad5dd7b020c2d0d849aae24baa580058fa61
Chino <2084049306@qq.com>
2022-05-07 10:26:24 +0800

    1

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>asd</div>
+  <Home />
 </template>
 
 <script setup>
+import { onMounted } from "vue";
+import Home from "./components/Home/index.vue";
+
+onMounted(() => {
+  if (document.getElementById("loading_warp")) {
+    document.getElementById("loading_warp").style.opacity = 0;
+    setTimeout(() => {
+      document.getElementById("loading_warp").remove();
+    }, 1000);
+  }
+});
 </script>
