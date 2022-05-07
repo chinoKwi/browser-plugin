f3ba106d71f6f2c468a20540b73cebd9b086e068
Chino <2084049306@qq.com>
2022-05-07 10:16:39 +0800

    更改项目

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+import 'ant-design-vue/dist/antd.css';
+
+import { message } from 'ant-design-vue';
+window.$message = message
+
+const app = createApp(App).use(store)
+
+app.mount('#app')
